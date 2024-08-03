0c4994224bbe01a9039f1e920ec1e4533265cf2a
DESKTOP-87FHOSD\sojib <bagchi.sojib777@gmail.com>
2024-08-03 21:31:27 +0530

    new repo created

---
+++ Practice_set_1/1st.py
@@ -0,0 +1,12 @@
+import pyjokes
+
+prompt=''
+
+prompt += '\nDo you want a new Jokes y(or anything)/n :'
+
+while (response := input(prompt).lower()) != 'n':
+    print('response is ',response)
+    if response=='y':
+        print(pyjokes.get_joke(language= "en",category= "neutral"))
+    else :
+        print('Please enter correct response')
